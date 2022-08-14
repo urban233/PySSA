724e1c74b84b480d3e2d1456be802d00772d1f84
Martin Urban <labgeek03@protonmail.com>
2022-08-14 16:13:42 +0200

    added license information

--- pymolproteintools/utility.py
+++ pymolproteintools/utility.py
@@ -1,30 +1,25 @@
-# ----------------------------------------------------------------------- #
-# Python Package: pymolproteintools
-# Version 1.0 for Python 3.9
-# ----------------------------------------------------------------------- #
-# Authors: Martin Urban & Hannah Kullik
-# Westfaelische Hochschule
-# Recklinghausen, Germany
 #
-# Copyright 2022 Martin Urban & Hannah Kullik
-#
-# Citation:
-# Martin Urban & Hannah Kullik, pymolproteintools (PPT),
-# Version 1.0, Recklinghausen, Germany, 2022.
-#
-#
+# PySSA - Python-Plugin for Sequence-to-Structure Analysis
+# Copyright (C) 2022
+# Martin Urban (martin.urban@studmail.w-hs.de)
+# Hannah Kullik (hannah.kullik@studmail.w-hs.de)
+# 
+# Source code is available at <https://github.com/urban233/PySSA>
+# 
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (LGPL) as
-# published by the Free Software Foundation, version 3 of the License.
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License (LGPL) for more details.
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program. If not, see
-# <http://www.gnu.org/licenses/>.
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-# ----------------------------------------------------------------------- #
 import pandas as pd
 from pymol import cmd
 import numpy as np
