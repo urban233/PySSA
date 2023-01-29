12421a69cb34a1bffaaeabf879c7d3a8b12daf33
martinurban <martin.urban@studmail.w-hs.de>
2023-01-29 11:54:54 +0100

    problematic changes

---
+++ testing_area/test_pyssa.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+
+class TestPyssa:
+
+    def __init__(self):
+        # moves to (519,1060) in 1 sec
+        pyautogui.moveTo(1536, 42, duration=1)
+
+        # simulates a click at the present
+        # mouse position
+        pyautogui.click()
