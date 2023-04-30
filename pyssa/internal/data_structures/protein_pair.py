a36ca90d0aff6c5ebc820db5f82d5b2df130fef8
hannah <hannah.kullik@studmail.w-hs.de>
2023-04-30 16:05:45 +0200

    fix: filepath for save_session_of_protein_pair in protein_pair.py

--- pyssa/internal/data_structures/protein_pair.py
+++ pyssa/internal/data_structures/protein_pair.py
@@ -153,7 +153,7 @@ class ProteinPair:
             The file name (filename) MUST NOT have the file extension .pse!
 
         """
-        protein_pair_operations.save_session_of_protein_pair(self.pymol_session_filepath)
+        protein_pair_operations.save_session_of_protein_pair(constants.CACHE_PYMOL_SESSION_DIR)
 
     def set_distance_analysis(self, value):
         self.distance_analysis = value
