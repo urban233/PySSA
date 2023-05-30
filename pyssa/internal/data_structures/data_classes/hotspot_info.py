f8d5939d9ae485d26b3a99b246bdf4aae5bf8aa0
hannah <hannah.kullik@studmail.w-hs.de>
2023-05-30 09:54:50 +0200

    try to fix hotspotpage

--- pyssa/internal/data_structures/data_classes/hotspot_info.py
+++ pyssa/internal/data_structures/data_classes/hotspot_info.py
@@ -30,3 +30,30 @@ class HotspotInfo:
     prot_2: str
     prot_2_seq_start: int
     prot_2_seq_end: int
+
+    def __init__(self, prot_1: str, prot_1_seq_start: int, prot_1_seq_end: int, prot_2: str, prot_2_seq_start: int,
+                 prot_2_seq_end: int):
+        self.prot_1 = prot_1
+        self.prot_1_seq_start = prot_1_seq_start
+        self.prot_1_seq_end = prot_1_seq_end
+        self.prot_2 = prot_2
+        self.prot_2_seq_start = prot_2_seq_start
+        self.prot_2_seq_end = prot_2_seq_end
+
+    def get_prot_1(self):
+        return self.prot_1
+
+    def get_prot_1_seq_start(self):
+        return self.prot_1_seq_start
+
+    def get_prot_1_seq_end(self):
+        return self.prot_1_seq_end
+
+    def get_prot_2(self):
+        return self.prot_2
+
+    def get_prot_2_seq_start(self):
+        return self.prot_2_seq_start
+
+    def get_prot_2_seq_end(self):
+        return self.prot_2_seq_end
