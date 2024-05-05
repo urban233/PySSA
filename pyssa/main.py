f237f4d7e23f5c8f572f92bbc58c60aa6169ff51
Martin Urban <martin.urban@studmail.w-hs.de>
2024-05-05 16:10:32 +0200

    add: splash screen

--- pyssa/main.py
+++ pyssa/main.py
@@ -21,11 +21,16 @@
 #
 """Module that is used to start PySSA."""
 import sys
+
 from PyQt5 import QtWidgets
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
+
 
 sys.path.append("C:\\ProgramData\\pyssa\\mambaforge_pyssa\\pyssa-mamba-env\\Lib\\site-packages\\pymol\\pymol_path\\data\\startup\\PySSA")
 sys.path.append("C:\\ProgramData\\pyssa\\mambaforge_pyssa\\pyssa-mamba-env\\Lib\\site-packages\\pymol\\pymol_path\\data\\startup\\PySSA\\pyssa")
 
+from pyssa.util import constants
 from pyssa.gui.ui.styles import styles
 from pyssa.controller import main_view_controller
 from pyssa.controller import interface_manager
@@ -33,10 +38,17 @@ from pyssa.controller import interface_manager
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    # setup QSplashScreen
+    pixmapi = QtGui.QPixmap(f"{constants.PLUGIN_PATH}\\assets\\images\\splash_screen.png")
+    smaller_pixmapi = pixmapi.scaled(700, 700, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+    tmp_splash = QtWidgets.QSplashScreen(smaller_pixmapi)
+    tmp_splash.show()
+    # Begin with PySSA startup
     styles.set_stylesheet(app)
     interfaceManager = interface_manager.InterfaceManager()
     main_window = interfaceManager.get_main_view()
     main_controller = main_view_controller.MainViewController(interfaceManager)
     styles.set_stylesheet_homepage(main_window)
     main_window.show()
+    tmp_splash.finish(None)
     sys.exit(app.exec_())
