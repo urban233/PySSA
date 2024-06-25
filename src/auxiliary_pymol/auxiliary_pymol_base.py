3fa78682b23cc4a84005c8615edc4dcf5a6ad658
Martin Urban <martin.urban@studmail.w-hs.de>
2024-06-25 23:05:50 +0200

    Fix: incorrect center of rotation for some protein/ protein pairs

--- src/auxiliary_pymol/auxiliary_pymol_base.py
+++ src/auxiliary_pymol/auxiliary_pymol_base.py
@@ -101,6 +101,7 @@ class AuxiliaryPyMOL:
                 auxiliary_pymol.cmd.color(local_constants.DEFAULT_COLOR_PROTEIN_2, tmp_protein_2_name)
 
                 auxiliary_pymol.cmd.zoom("all")
+                auxiliary_pymol.cmd.origin()
                 auxiliary_pymol.cmd.scene("base", action="store")
 
                 auxiliary_pymol.cmd.bg_color(local_constants.PYMOL_DEFAULT_BACKGROUND_COLOR)
@@ -403,6 +404,7 @@ class AuxiliaryPyMOL:
                 auxiliary_pymol.cmd.unset("depth_cue")
                 auxiliary_pymol.cmd.color("green", tmp_protein_name)
                 auxiliary_pymol.cmd.reset()
+                auxiliary_pymol.cmd.origin()
                 auxiliary_pymol.cmd.scene("base", action="store")
                 session_filepath: pathlib.Path = pathlib.Path(
                     f"{local_constants.SCRATCH_DIR}/{tmp_protein_name}_session.pse",
