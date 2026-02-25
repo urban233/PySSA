ca0fad3e95a5def1c2cfa344c4c25095208aaf36
Hannah Kullik <hannah.kullik@studmail.w-hs.de>
2026-02-25 20:46:21 +0100

    add: introduce hover help for selection and hide all menus; update related HTML for clarity and consistency

--- src/pyssa/controller/main_window_controller.py
+++ src/pyssa/controller/main_window_controller.py
@@ -238,7 +238,9 @@ class MainWindowController:
             ("dots",            "viewer_dots"),
             ("mesh",            "viewer_mesh"),
             ("surface",         "viewer_surface"),
+            ("hide_all",        "viewer_hide_all_repr"),
             ("color",           "viewer_color"),
+            ("selection",       "viewer_selection"),
             ("clean",           "viewer_clean"),
             ("running_jobs",    "viewer_running_jobs"),
             ("notifications",   "viewer_notifications"),
@@ -267,6 +269,10 @@ class MainWindowController:
             _menu.hovered.connect(self.help_filter.handle_menu_action_hovered)
             _menu.aboutToHide.connect(self.help_filter.handle_menu_about_to_hide)
         logger.info("Connected hover help for all representation show/hide menus.")
+
+        # Connect the Selection popup menu.
+        _main_window.selection_show_hide_menu.hovered.connect(self.help_filter.handle_menu_action_hovered)
+        _main_window.selection_show_hide_menu.aboutToHide.connect(self.help_filter.handle_menu_about_to_hide)
 
         # Connect the Clean popup menu.
         _main_window.clean_solvent_organic_menu.hovered.connect(self.help_filter.handle_menu_action_hovered)
