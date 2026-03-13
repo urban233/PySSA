0ffe93c5e3c1a7877e1f9da8fdb10df47ba0f235
Martin Urban <martin.urban@studmail.w-hs.de>
2026-03-13 17:13:35 +0100

    Update `start_pyssa.bat` for proper execution flow and modify demo database files

--- deployment/resources/start_pyssa.bat
+++ deployment/resources/start_pyssa.bat
@@ -1,1 +1,3 @@
-%USERPROFILE%\AppData\Local\IBCI\PySSA\cpython-3.11.14\python\pythonw.exe  %USERPROFILE%\AppData\Local\IBCI\PySSA\cpython-3.11.14\python\Lib\site-packages\src\pyssa\__main__.py
+@echo off
+start "" "%USERPROFILE%\AppData\Local\IBCI\PySSA\cpython-3.11.14\python\pythonw.exe"  "%USERPROFILE%\AppData\Local\IBCI\PySSA\cpython-3.11.14\python\Lib\site-packages\src\pyssa\__main__.py"
+exit
